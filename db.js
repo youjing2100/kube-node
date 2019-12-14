53b5e93a0563736d3f8867d118507e0c72126ac7
You Jing Lee <youjing.lee@goviewpoint.com>
2019-12-15 01:01:11 +0800

    update

--- db.js
+++ db.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
 
-const MONGO_USERNAME = 'sammy';
-const MONGO_PASSWORD = 'password';
-const MONGO_HOSTNAME = '127.0.0.1';
-const MONGO_PORT = '27017';
-const MONGO_DB = 'sharkinfo';
+const {
+    MONGO_USERNAME,
+    MONGO_PASSWORD,
+    MONGO_HOSTNAME,
+    MONGO_PORT,
+    MONGO_DB
+} = process.env;
+
+const option = {
+    useNewUrlParser: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500,
+    connectTimeoutMS: 10000,
+}
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
-mongoose.connect(url, {useNewUrlParser: true});
+mongoose.connect(url, option).then(function(){
+    console.log('MongoDB is connected');
+}).catch(function(err) {
+    console.log(err);
+});
